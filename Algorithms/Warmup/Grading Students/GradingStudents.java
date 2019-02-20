d84b457552861fa64d98813d1bc7bf0a44623dd0
Lewis Duncan <lewisduncan93@gmail.com>
2019-02-20 03:04:23 +0000

    Added Grading Students

---
+++ Algorithms/Warmup/Grading Students/GradingStudents.java
@@ -0,0 +1,56 @@
+package Warmup;
+
+import java.io.*;
+import java.math.*;
+import java.text.*;
+import java.util.*;
+import java.util.regex.*;
+
+/**
+ * https://www.hackerrank.com/challenges/grading/problem
+ *
+ */
+
+public class GradingStudents {
+
+    static int[] gradingStudents(int[] grades) {
+
+        for(int i = 0; i < grades.length; i++){
+            if(grades[i] >= 38){
+                int roundedFive = 5 + grades[i] - (grades[i] % 5);
+                if(roundedFive - grades[i] < 3){
+                    grades[i] = roundedFive;
+                }
+            }
+        }
+        return grades;
+    }
+
+    private static final Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(scan.nextLine().trim());
+
+        int[] grades = new int[n];
+
+        for (int gradesItr = 0; gradesItr < n; gradesItr++) {
+            int gradesItem = Integer.parseInt(scan.nextLine().trim());
+            grades[gradesItr] = gradesItem;
+        }
+
+        int[] result = gradingStudents(grades);
+
+        for (int resultItr = 0; resultItr < result.length; resultItr++) {
+            bw.write(String.valueOf(result[resultItr]));
+
+            if (resultItr != result.length - 1) {
+                bw.write("\n");
+            }
+        }
+        bw.newLine();
+
+        bw.close();
+    }
+}
